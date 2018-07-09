930aaef5ae90f6f2ba84dcb2ec46131ec2737ff8
chauhannishith <chauhannis123ith@gmail.com>
2018-07-09 09:56:30 +0530

    added readme for reference when installing package

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 # groceristar-fetch
 fetch static data from files and return array to use in recipe search
 
+var gf = require('@groceristar/groceristar-fetch')
+
+var allergies = gf.getAllergies();
+returns array of allergies
+
+var courses = module.getCourses();
+returns array of courses
+
+var cuisines = module.getCuisines();
+returns array of cuisines
+
+var diets = module.getDiets();
+returns array of diets
+
+var holidays = module.getHolidays();
+returns array of holidays
+
+var ingredients = module.getIngredients();
+returns array of primary ingredients
+
+var ingredients2 = module.getIngredients2();
+returns array of some more ingredients
+
+var Measurements = module.getMeasurements();
+returns array of measurements object containing singular, plural and abbreviation
+example
+{
+    "singular": "gram",
+    "plural": "grams",
+    "abbreviation": "g"
+}
+
+
 [![Build Status](https://travis-ci.org/GroceriStar/groceristar-fetch.svg?branch=master)](https://travis-ci.org/GroceriStar/groceristar-fetch)
--- index.test.js
+++ index.test.js
@@ -1,37 +1,45 @@
 const index = require('./index.js')
 
-test('allergies file is set', () => {
-  var stream = index.getAllergies()//fs.readFileSync(allergies, 'utf-8')
-  expect(stream).not.toBe('')
 
-})
+describe('testing index.js file', () => {
+  test('allergies file is set', () => {
+    var stream = index.getAllergies()//fs.readFileSync(allergies, 'utf-8')
+    expect(stream).not.toBe('')
 
-test('courses file is set', () => {
-  var stream = index.getCourses()//fs.readFileSync(courses, 'utf-8')
-  expect(stream).not.toBe('')
-})
+  })
 
-test('cuisines file is set', () => {
-  var stream = index.getCuisines()//fs.readFileSync(cuisines, 'utf-8')
-  expect(stream).not.toBe('')
-})
+  test('courses file is set', () => {
+    var stream = index.getCourses()//fs.readFileSync(courses, 'utf-8')
+    expect(stream).not.toBe('')
+  })
 
-test('diets file is set', () => {
-  var stream = index.getDiets()//fs.readFileSync(diets, 'utf-8')
-  expect(stream).not.toBe('')
-})
+  test('cuisines file is set', () => {
+    var stream = index.getCuisines()//fs.readFileSync(cuisines, 'utf-8')
+    expect(stream).not.toBe('')
+  })
 
-test('holidays file is set', () => {
-  var stream = index.getHolidays()//fs.readFileSync(holidays, 'utf-8')
-  expect(stream).not.toBe('')
-})
+  test('diets file is set', () => {
+    var stream = index.getDiets()//fs.readFileSync(diets, 'utf-8')
+    expect(stream).not.toBe('')
+  })
 
-test('ingredients file is set', () => {
-  var stream = index.getIngredients()//fs.readFileSync(ingredients, 'utf-8')
-  expect(stream).not.toBe('')
-})
+  test('holidays file is set', () => {
+    var stream = index.getHolidays()//fs.readFileSync(holidays, 'utf-8')
+    expect(stream).not.toBe('')
+  })
 
-test('measurements file is set', () => {
-  var stream = index.getMeasurements()//fs.readFileSync(measurements, 'utf-8')
-  expect(stream).not.toBe('')
+  test('ingredients file is set', () => {
+    var stream = index.getIngredients()//fs.readFileSync(ingredients, 'utf-8')
+    expect(stream).not.toBe('')
+  })
+
+  test('ingredients2 file is set', () => {
+    var stream = index.getIngredients2()//fs.readFileSync(ingredients, 'utf-8')
+    expect(stream).not.toBe('')
+  })
+
+  test('measurements file is set', () => {
+    var stream = index.getMeasurements()//fs.readFileSync(measurements, 'utf-8')
+    expect(stream).not.toBe('')
+  })
 })
