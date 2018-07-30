a5f66c35e9d040bbb5622918c5235b2696d97e24
Vadim <vadim.putrov@gmail.com>
2018-07-30 10:03:34 +0300

    connected new GL

--- search.js
+++ search.js
@@ -3,8 +3,8 @@ const courses       = require('./data/Course/courses.json');
 const cuisines      = require('./data/Cuisine/cuisines.json');
 const diets         = require('./data/Diet/diets.json');
 const holidays      = require('./data/Holiday/holidays.json');
-const ingredients1  = require('./data/Ingredient/ingredients1.json');
-const ingredients2  = require('./data/Ingredient/ingredients2.json');
+const ingredients1  = require('./data/Ingredients/ingredients1.json');
+const ingredients2  = require('./data/Ingredients/ingredients2.json');
 const measurements  = require('./data/Measurement/measurements.json');
 
 const path = require('path')
