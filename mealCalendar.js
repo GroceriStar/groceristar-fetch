25ffdfa1a5cc0bae0bbcdc1f641260e9555ef756
Vadim <vadim.putrov@gmail.com>
2018-09-08 15:47:12 +0300

    fix

--- mealCalendar.js
+++ mealCalendar.js
@@ -9,16 +9,30 @@ module.getMealCalendar = function () {
   return parser(mealCalendar);
 }
 
-module.getDishByDay = function(day, maxDishes = mealCalendar.length) {
-  return _.filter(parser(mealCalendar), (meal, index) => {
-    if (index < maxDishes) {
-      return {
-        ...meal,
-        recipes: _.filter(meal.recipes, { day })
-      };
-    }
-  });
-};
+// module.getDishByDay = function(day, maxDishes = mealCalendar.length) {
+//   return _.filter(parser(mealCalendar), (meal, index) => {
+//     if (index < maxDishes) {
+//       return {
+//         ...meal,
+//         recipes: _.filter(meal.recipes, { day })
+//       };
+//     }
+//   });
+// };
+
+module.getDishByDay = function(day){
+  let result =[];
+   _.map(mealCalendar, function(item){
+    return _.filter(item["recipes"],function(arr){
+      if(arr[0].day === day){
+        arr[0].id = item.id;
+        result.push(arr[0])
+        }
+    })
+
+  })
+  return result;
+}
 
 module.getDishByMonth = function(monthNumb) {
   if (monthNumb < 1 || monthNumb > 5) return [];
