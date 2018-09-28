c08e64decb4789f4f9b6e0ebcd65aefb107f47d4
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-09-28 03:11:10 +0300

    +++

--- index.js
+++ index.js
@@ -6,6 +6,42 @@ const mealCalendar = require('./mealCalendar.js');
 // groceristar.count();
 
 // module.count = function() {
+
+// ing 619
+// dep 58
+// grocery 7
+// users 3
+
+// dep
+// [
+//   {
+//     "name": "Fresh vegetables",
+//     "type": "food"
+//   },
+
+// grocery
+// [{
+//     "departments": [
+//     ],
+//     "id": 1,
+//     "name": "Ultimate Grocery List",
+//     "img": false,
+//     "desc": false,
+//     "slug": false
+
+// ingredient
+// [{
+//     "name": "Asparagus",
+//     "department": "Fresh vegetables"
+//   },
+
+// users
+// [{
+//      "name": "john",
+//      "email": "john.doe@ibm.com",
+//      "password": "john1"
+//    },
+
 const _ = require('lodash')
 	console.log('ingredient ' +  _.size(groceristar.getIngredients() ) )
 	console.log('department ' +  _.size(groceristar.getDepartments() ) )
