f71c6bf837ec267ccc81a99a5c11ce6f55dfa56a
GuiFSs <devguisantos@gmail.com>
2018-10-20 10:34:24 -0300

    fixed data path

--- projects/Search/search.js
+++ projects/Search/search.js
@@ -1,11 +1,11 @@
-const allergies     = require('./data/Allergy/allergies.json');
-const courses       = require('./data/Course/courses.json');
-const cuisines      = require('./data/Cuisine/cuisines.json');
-const diets         = require('./data/Diet/diets.json');
-const holidays      = require('./data/Holiday/holidays.json');
-const ingredients1  = require('./data/Ingredients/ingredients1.json');
-const ingredients2  = require('./data/Ingredients/ingredients2.json');
-const measurements  = require('./data/Measurement/measurements.json');
+const allergies     = require('../../data/Allergy/allergies.json');
+const courses       = require('../../data/Course/courses.json');
+const cuisines      = require('../../data/Cuisine/cuisines.json');
+const diets         = require('../../data/Diet/diets.json');
+const holidays      = require('../../data/Holiday/holidays.json');
+const ingredients1  = require('../../data/Ingredients/ingredients1.json');
+const ingredients2  = require('../../data/Ingredients/ingredients2.json');
+const measurements  = require('../../data/Measurement/measurements.json');
 
 const path = require('path')
 
