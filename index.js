75d68bbbe501e6d949347e37812bb4e179dc7c31
GuiFSs <devguisantos@gmail.com>
2018-10-24 19:31:25 -0300

    fixed issue #109

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const groceristar  = require("./projects/GroceriStar/groceristar.js");
 const chickenKyiv  = require('./projects/ChickenKyiv/chickenKyiv.js');
 const mealCalendar = require("./projects/MealCalendar/mealCalendar.js");
 
-// maybe replace it and test
-// {
-//   search: search,
-//   groceristar: groceristar,
-//   chickenKyiv: chickenKyiv,
-//   mealCalendar: mealCalendar
-// }
-module.exports = module;
+module.exports = {
+  search: search,
+  groceristar: groceristar,
+  chickenKyiv: chickenKyiv,
+  mealCalendar: mealCalendar
+}
