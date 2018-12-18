ceed5d14319447e47b32c80c330deb6e712e3e05
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-18 07:56:59 +0200

    Update gl-object-copy.js

--- data/Grocery/gl-object-copy.js
+++ data/Grocery/gl-object-copy.js
@@ -1,6 +1,6 @@
 //it's an actual object, that contain all information.
-so it can be added to our array of grocery lists and will work fine.
-i separate it - because it's a better approach and it can be easily tested
+//so it can be added to our array of grocery lists and will work fine.
+//i separate it - because it's a better approach and it can be easily tested
 
 {
     "departments": [
@@ -10,10 +10,10 @@ i separate it - because it's a better approach and it can be easily tested
     
     
     "id": ,
-    "name": "",
+    "name": "19 Gluten-Free Foods Shopping List",
     "img": false,
     "desc": false,
     "slug": false
  }
  
- later coma will be important. keep it in mind.
+ //later coma will be important. keep it in mind.
