57f8b88145fb2d59b55db5dff2dd0033b116e1b4
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-18 07:55:49 +0200

    Create gl-object-copy.js

---
+++ data/Grocery/gl-object-copy.js
@@ -0,0 +1,19 @@
+//it's an actual object, that contain all information.
+so it can be added to our array of grocery lists and will work fine.
+i separate it - because it's a better approach and it can be easily tested
+
+{
+    "departments": [
+      "Fresh vegetables",  
+      "Other"
+    ],
+    
+    
+    "id": ,
+    "name": "",
+    "img": false,
+    "desc": false,
+    "slug": false
+ }
+ 
+ later coma will be important. keep it in mind.
