c2a71ab4c32b51117538e924c0b3e307caaafa3b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-12-27 13:49:05 +0200

    Update search.js

--- projects/Search/search.js
+++ projects/Search/search.js
@@ -43,22 +43,79 @@ module.getAttribute = function (attribute) {
   }
 }
 
-module.getPlaceholder = function(attribute) {
-  if (attribute == 'Allergy') {
+module.getPlaceholder = function(attribute, flag = false) {
+  if (attribute == 'allergy') {
     return "Allergies";
   }
-  if (attribute == 'Diets') {
+  if (attribute == 'diets') {
     return "Specific Diets";
   }
-  if (attribute == 'Cuisine') {
+  if (attribute == 'cuisine') {
     return "Specific Cuisine ";
   }
-  if (attribute == 'Course') {
+  if (attribute == 'course') {
     return "Course";
   }
-  if (attribute == 'Holidays') {
+  if (attribute == 'holidays') {
     return "Holiday";
   }
+  // case for ingredients, not for attributes...  
+  if( attribute == 'ingredient' ) {
+     return  ( flag ) ? "Ingredients you have" : "Ingredients you don't have";
+  }  
+    
+    
+// @TODO use this instead of ifs
+// switch (attribute) {
+//   case 'allergies':
+//     return parser(allergies);
+//     break;
+//   case 'cuisines':
+//     return parser(cuisines);
+//     break;
+//   case 'courses':
+//     return parser(courses);
+//     break;
+//   case 'holidays':
+//     return parser(holidays);
+//     break;
+//   case 'diets':
+//     return parser(diets);
+//     break;
+// 
+//   default: return "Incorrect attribute or empty argument";
+// }
+    
+}
+
+
+module.getIngredientsRelatedToSearch = function() {
+	return parser( ingredients1 )
+}
+
+// for antD version of select field we should have a method that return values as this example
+// const options = [
+//   { value: 'chocolate',  label: 'Chocolate'},
+//   { value: 'strawberry', label: 'Strawberry'},
+//   { value: 'vanilla',    label: 'Vanilla'  },
+//   { value: 'vanilla-ice',    label: 'Vanilla Ice'},
+//   { value: 'vanilla latte',    label: 'Vanilla Latte'},
+//   { value: 'vanilla Chino',    label: 'Vanilla Chino'},
+//   { value: 'vanilla double',    label: 'Vanilla Double' }
+// ];
+// Note: let's discuss a a different ways to handle values.
+// we can keep it as in our example - but for some cases you'll need to have a function that will convert a label into a value.
+// or we can connect an key-generator and use it similar to read ID values. 
+// Both options are ok for me.
+
+// @TODO check if this method will work fine with situation where we have disabled values.
+// @TODO check if this structure can be passed at React-Select module as well and 
+// will it generate a working select or not - I assume no, but we should test it
+// we'll use it at recipe-search-react/SearchForm.js
+module.getOptionsForSelectFieldV1(attribute) = function(){
+    const data = getAttribute(attribute);
+    console.log(data);
+    //....
 }
 
 module.exports = module;
