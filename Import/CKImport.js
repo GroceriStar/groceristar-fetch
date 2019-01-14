a66f252e129751d780a59e6e3e0d5bd7c50e2374
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-15 01:07:34 +0200

    upd

--- Import/CKImport.js
+++ Import/CKImport.js
@@ -1,10 +1,50 @@
 const _            = require('lodash');
 const chickenKyiv  = require('./projects/ChickenKyiv/chickenKyiv.js');
 
-const a = () => {
+
+
+
+
+// get('menu');
+// get('recipe');
+// get('ingredient');
+// get('recipe2');
+// get('department');
+
+const get = (tableName) => {
+
+  let result
+  switch (tableName) {
+    case 'menu':
+      result = chickenKyiv.getMenus();
+      break;
+
+    case 'recipe':
+      result = chickenKyiv.getRecipes();
+      break;
+
+    case 'ingredient':
+      result = chickenKyiv.getIngredients();
+      break;
+
+    case 'recipe2':
+      result = chickenKyiv.getRecipesExtended();
+      break;
+
+    case 'department':
+      result = chickenKyiv.getDepartment();
+      break;
+
+
+    default:
+      result = chickenKyiv.getUsers();
+
+  }
+
+  return result
 
 }
 
 module.exports = {
-  a:a
+  get:get
 }
