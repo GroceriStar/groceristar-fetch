18491866c60eb53e5bc32583bbf07409c546c84f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-15 00:33:40 +0200

    tested api methods

---
+++ Import/CKImport.js
@@ -0,0 +1,10 @@
+const _            = require('lodash');
+const chickenKyiv  = require('./projects/ChickenKyiv/chickenKyiv.js');
+
+const a = () => {
+
+}
+
+module.exports = {
+  a:a
+}
--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const chickenKyiv  = require('./projects/ChickenKyiv/chickenKyiv.js');
 const mealCalendar = require("./projects/MealCalendar/mealCalendar.js");
 // const showcase = require("./projects/MealCalendar/showcase.js");
 
+
+// i'm separating this file because it's not fit to logic of this module very well.
+const CKImport     = require("./Import/CKImport")
+
 module.exports = {
   search: search,
   groceristar: groceristar,
   chickenKyiv: chickenKyiv,
-  mealCalendar: mealCalendar
+  mealCalendar: mealCalendar,
+
+
+
+
+  CKImport: CKImport
 }
