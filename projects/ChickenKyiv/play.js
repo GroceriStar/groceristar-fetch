6198c5bcea059240bbc7b970659d28622a97ecbc
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-17 03:10:56 +0200

    recipe api...

--- projects/ChickenKyiv/play.js
+++ projects/ChickenKyiv/play.js
@@ -1,7 +1,6 @@
 const _            = require('lodash');
 
 const chickenKyiv  = require('../ChickenKyiv/chickenKyiv');
-// const search       = require('../Search/search');
 
 // console.log( chickenKyiv.getShowcaseFiveRecipes() );
 
@@ -10,8 +9,8 @@ const chickenKyiv  = require('../ChickenKyiv/chickenKyiv');
 // console.log( chickenKyiv. );
 
 
-
-// console.log( chickenKyiv.getNRecipes(6) );
+console.log( count(chickenKyiv.getRecipes()) );
+console.log( chickenKyiv.getNRecipes(6) );
 
 
 // console.log( chickenKyiv.getRandomRecipe() );
