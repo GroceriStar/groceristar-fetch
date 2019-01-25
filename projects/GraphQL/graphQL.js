d0626d4e9375879002f0d2c3577b95e110f6d92f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-25 15:50:20 +0200

    +

--- projects/GraphQL/graphQL.js
+++ projects/GraphQL/graphQL.js
@@ -63,7 +63,7 @@ const getIngredientsGraphQL = function(limit = false){
   if( limit ){
     results = sliceArray(results, 100);
   }
-  
+
   return results.map((item, index) => ({
     ingredient_id: ++index,
     favs:'',
@@ -117,6 +117,28 @@ const getUsersGraphQLKey = function(){
     }))
 };
 
+//---------
+// As we move our methods away we don't need GraphQL in methods name anymore ...
+const getUsersGrocery = function(){
+  // let results = groceristar.getUsers();
+  // return results.map((item, index) => ({
+  //   userId: ++index,
+  //   favs: false,
+  //   ingredient_id: 1,
+  //   grocery_id: 1
+  //   }))
+};
+
+const getUsersGroceryKey = function(){
+  // let results = groceristar.getUsers();
+  // return results.map((item, index) => ({
+  //   userId: uuidv1(),
+  //   favs: false,
+  //   ingredient_id: 1,
+  //   grocery_id: 1
+  //   }))
+};
+
 module.exports = {
   getDepartmentsGraphQL,
   getDepartmentsGraphQLKey,
@@ -126,4 +148,7 @@ module.exports = {
   getIngredientsGraphQLKey,
   getUsersGraphQL,
   getUsersGraphQLKey,
+
+  getUsersGrocery,
+  getUsersGroceryKey
 }
