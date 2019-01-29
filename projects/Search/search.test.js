e6af8ac28810a72af2cc2587ad6538540f74d607
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-30 00:17:01 +0200

    Update search.test.js

--- projects/Search/search.test.js
+++ projects/Search/search.test.js
@@ -2,27 +2,27 @@ const search      = require('./search.js')
 
 describe('testing static data files are set', () => {
 
-  test('getAttribute("allergy")', ()=> {
+  test('getAttribute("allergy")', () => {
     var stream = search.getAttribute('allergies')
     expect(stream).not.toBe('')
   })
 
-  test('getAttribute("cuisine")', ()=> {
+  test('getAttribute("cuisine")', () => {
     var stream = search.getAttribute('cuisines')
     expect(stream).not.toBe('')
   })
 
-  test('getAttribute("course")', ()=> {
+  test('getAttribute("course")', () => {
     var stream = search.getAttribute('courses')
     expect(stream).not.toBe('')
   })
 
-  test('getAttribute("holidays")', ()=> {
+  test('getAttribute("holidays")', () => {
     var stream = search.getAttribute('allergies')
     expect(stream).not.toBe('')
   })
 
-  test('getAttribute("diets")', ()=> {
+  test('getAttribute("diets")', () => {
     var stream = search.getAttribute('diets')
     expect(stream).not.toBe('')
   })
@@ -42,27 +42,27 @@ describe('testing static data files are set', () => {
   //   expect(stream).not.toBe('')
   // })
 
-  test('getPlaceholder("Allergy")', ()=> {
+  test('getPlaceholder("Allergy")', () => {
     var stream = search.getPlaceholder('Allergy')
     expect(stream).not.toBe('')
   })
 
-  test('getPlaceholder("Diets")', ()=> {
+  test('getPlaceholder("Diets")', () => {
     var stream = search.getPlaceholder('Diets')
     expect(stream).not.toBe('')
   })
 
-  test('getPlaceholder("Cuisine")', ()=> {
+  test('getPlaceholder("Cuisine")', () => {
     var stream = search.getPlaceholder('Cuisine')
     expect(stream).not.toBe('')
   })
 
-  test('getPlaceholder("Course")', ()=> {
+  test('getPlaceholder("Course")', () => {
     var stream = search.getPlaceholder('Course')
     expect(stream).not.toBe('')
   })
 
-  test('getPlaceholder("Holidays")', ()=> {
+  test('getPlaceholder("Holidays")', () => {
     var stream = search.getPlaceholder('Holidays')
     expect(stream).not.toBe('')
   })
