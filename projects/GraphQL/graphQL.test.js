b0ca58fc52f8fecb2715a4a371afcd405f4789cf
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-29 23:32:06 +0200

    Update graphQL.test.js

--- projects/GraphQL/graphQL.test.js
+++ projects/GraphQL/graphQL.test.js
@@ -1,19 +1,63 @@
 const graphQL = require('./graphQL.js');
 
-describe("graphQL methods should return data",() => {
-  test("getFavorites()", () => {
+describe("graphQL methods should return data", () => {
+  
+  test("method getDepartmentsGraphQL()", () => {
+    var stream = graphQL.getDepartmentsGraphQL();
+    expect(stream).not.toBe("");
+  })
+     
+ test("method getDepartmentsGraphQLKey()", () => {
+    var stream = graphQL.getDepartmentsGraphQLKey();
+    expect(stream).not.toBe("");
+  }) 
+
+   
+ test("method getGroceryGraphQL()", () => {
+    var stream = graphQL.getGroceryGraphQL();
+    expect(stream).not.toBe("");
+  })  
+    
+  test("method getGroceryGraphQLKey()", () => {
+    var stream = graphQL.getGroceryGraphQLKey();
+    expect(stream).not.toBe("");
+  })    
+    
+  test("method getIngredientsGraphQL()", () => {
+    var stream = graphQL.getIngredientsGraphQL();
+    expect(stream).not.toBe("");
+  })     
+  
+  test("method getIngredientsGraphQLKey()", () => {
+    var stream = graphQL.getIngredientsGraphQLKey();
+    expect(stream).not.toBe("");
+  })   
+  
+  test("method getUsersGraphQL()", () => {
+    var stream = graphQL.getUsersGraphQL();
+    expect(stream).not.toBe("");
+  })   
+     
+  test("method getUsersGraphQLKey()", () => {
+    var stream = graphQL.getUsersGraphQLKey();
+    expect(stream).not.toBe("");
+  })   
+ 
+  
+    
+  test("method getFavorites()", () => {
     var stream = graphQL.getFavorites();
     // console.log(graphQL.getUserGroceryGraphQL());
     // console.log(graphQL.getUserGroceryGraphQLKey());
     expect(stream).not.toBe("");
   })
 
-  test("getItems()", () => {
+  test("method getItems()", () => {
     var stream = graphQL.getItems();
     expect(stream).not.toBe("");
   })
 
-  test("getUserGrocery()", () => {
+  test("method getUserGrocery()", () => {
     var stream = graphQL.getUserGrocery();
     expect(stream).not.toBe("");
   })
