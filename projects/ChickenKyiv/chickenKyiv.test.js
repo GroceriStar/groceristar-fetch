ca1fe6c4ce62dc8e6fd810f94b625e7f3f3a8912
Arthur Tkachenko <atherdon@gmail.com>
2019-02-02 13:55:09 +0200

    clean up

--- projects/ChickenKyiv/chickenKyiv.test.js
+++ projects/ChickenKyiv/chickenKyiv.test.js
@@ -2,20 +2,6 @@ const chickenKyiv = require('./chickenKyiv.js')
 
 describe('testing static data files are set', () => {
 
-  // test('ingredients3 file is set', () => {
-  //   var result = chickenKyiv.getIngredients3()
-  //   expect(result).not.toBe('')
-  // })
-
-  // test('menu file is set', () => {
-  //   var result = chickenKyiv.getMenu()
-  //   expect(result).not.toBe('')
-  // })
-
-  // test('recipe file is set', () => {
-  //   var result = chickenKyiv.getRecipe()
-  //   expect(result).not.toBe('')
-  // })
 
   test('generate basic Weekly Menu objects', () => {
     var result = chickenKyiv.getMenuGenerator(1)
@@ -33,6 +19,7 @@ describe('testing static data files are set', () => {
     expect(result).not.toBe('')
   })
 
+
   test('getFiveRandomIngredients', () => {
     var result = chickenKyiv.getFiveRandomIngredients()
     expect(result).not.toBe('')
@@ -43,15 +30,8 @@ describe('testing static data files are set', () => {
     expect(result).not.toBe('')
   })
 
-  test('nutritions1 file is set', () => {
-    var result = chickenKyiv.getNutritions1()
-    expect(result).not.toBe('')
-  })
 
-  test('nutritions2 file is set', () => {
-    var result = chickenKyiv.getNutritions2()
-    expect(result).not.toBe('')
-  })
+
 
 
 })
--- projects/Showcase/showcase.test.js
+++ projects/Showcase/showcase.test.js
@@ -1,9 +1,10 @@
 const showcase = require("./showcase.js");
 
 describe("Showcase project methods should return data", () => {
+
   test("method getGroceryShowcase()", () => {
     var result = showcase.getGroceryShowcase();
     expect(result).not.toBe("");
   });
-}
-)
+
+})
