6b8b028c24594ea8c41c614ff3bb93460e413b39
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-03 23:51:01 +0200

    Update getGroceriesWithDepIngKey.test.js

--- projects/GroceriStar/getGroceriesWithDepIngKey.test.js
+++ projects/GroceriStar/getGroceriesWithDepIngKey.test.js
@@ -1,2 +1,8 @@
 //This will be our separated file with tests, related to this method
 // main test file are huuuge, so let's keep new tests here
+
+const groceristar = require("./groceristar.js");
+
+describe("testing ", () => {
+  test("  method ", () => { })
+})
