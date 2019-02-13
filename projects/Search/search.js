00c826b3e033f317614c96103a6633cba923942b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-13 22:41:34 +0200

    +++

--- projects/Search/search.js
+++ projects/Search/search.js
@@ -18,6 +18,7 @@ const {
 // const measurements  = require('../../data/Measurement/measurements.json');
 
 // yes it's bad, but it works, some sort of...
+//@TODO https://github.com/GroceriStar/sd/issues/3
 const allergies     = require('@groceristar/sd-wrapper/lib/data/Allergy/allergies.json');
 const courses       = require('@groceristar/sd-wrapper/lib/data/Course/courses.json');
 const cuisines      = require('@groceristar/sd-wrapper/lib/data/Cuisine/cuisines.json');
