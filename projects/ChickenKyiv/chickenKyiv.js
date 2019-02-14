05a495bcc90d1fde65e13747d2e7d466963d6338
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-15 01:49:05 +0200

    roll back?

--- projects/ChickenKyiv/chickenKyiv.js
+++ projects/ChickenKyiv/chickenKyiv.js
@@ -4,6 +4,18 @@ const dayjs        = require('dayjs');
 
 const { parser, sliceArray }   = require('../../helper');
 
+const ingredients3 = require('../../data/Ingredients/ingredients3.json');
+// const ingredients  = require('../../data/Ingredients/ingredients.json');
+
+const menus        = require('../../data/Menu/menu.json');
+const recipes      = require('../../data/Recipe4/recipe.json');
+const nutritions1  = require('../../data/Nutrition/nutritions1.json');
+const nutritions2  = require('../../data/Nutrition/nutritions2.json');
+
+
+const departments  = require('../../data/Departments/departments.json');
+const users        = require('../../data/Users/users.json');
+
 const getIngredients3 = function() {
   return parser(ingredients3)
 }
