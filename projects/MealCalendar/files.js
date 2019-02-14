163902b5b50e67c013534bfe8c6698d272407691
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-14 03:07:54 +0200

    ++++

--- projects/MealCalendar/files.js
+++ projects/MealCalendar/files.js
@@ -1,8 +1,8 @@
-const mealCalendar   = require('../../data/MealCalendar/mealcalendar.json');
-// const ingredients3     = require('@groceristar/sd-wrapper/lib/data/Ingredients/ingredients3.json');
+const mealCalendar     =
+  require('@groceristar/sd-wrapper/lib/data/MealCalendar/mealcalendar.json');
 
-const firstVeganGLMC = require('../../data/MealCalendar/First-Vegan-Grocery-List-MC.json');
-// const ingredients3     = require('@groceristar/sd-wrapper/lib/data/Ingredients/ingredients3.json');
+const firstVeganGLMC     = 
+  require('@groceristar/sd-wrapper/lib/data/MealCalendar//First-Vegan-Grocery-List-MC.json');
 
 
 
