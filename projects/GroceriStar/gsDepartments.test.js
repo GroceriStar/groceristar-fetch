6cb6ba48a2ac28562b303eb3eee9c7abe9c8530a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-17 04:22:49 +0200

    separating tests, related to groceristar proj

---
+++ projects/GroceriStar/gsDepartments.test.js
@@ -0,0 +1,36 @@
+const groceristar = require("./groceristar.js");
+
+
+describe("testing objects inside departments", () => {
+  test("each object have property 'name'", () =>{
+    var result = groceristar.getDepartments();
+    result.forEach((obj) => {
+      expect(obj).toHaveProperty("name");
+    })
+  })
+
+  test("each object have property 'type'", () =>{
+    var result = groceristar.getDepartments();
+    result.forEach((obj) => {
+      expect(obj).toHaveProperty("type");
+    })
+  })
+
+  test("objects property 'name' is String", () =>{
+    var result = groceristar.getDepartments();
+    result.forEach((obj) => {
+      expect(obj).toMatchObject({
+        name: expect.any(String)
+      });
+    })
+  })
+
+  test("objects property 'type' is String", () => {
+    var result = groceristar.getDepartments();
+    result.forEach((obj) => {
+      expect(obj).toMatchObject({
+        type: expect.any(String)
+      });
+    })
+  })
+})
---
+++ projects/GroceriStar/gsUsers.test.js
@@ -0,0 +1,53 @@
+const groceristar = require("./groceristar.js");
+
+
+describe("testing objects inside users", () => {
+  test("each object have property 'name'",() =>{
+    var result = groceristar.getUsers();
+    result.forEach((obj) => {
+      expect(obj).toHaveProperty("name")
+    })
+  })
+
+  test("each object have property 'email'",() =>{
+    var result = groceristar.getUsers();
+    result.forEach((obj) => {
+      expect(obj).toHaveProperty("email")
+    })
+  })
+
+  test("each object have property 'password'",() =>{
+    var result = groceristar.getUsers();
+    result.forEach((obj) => {
+      expect(obj).toHaveProperty("password")
+    })
+  })
+
+  test("objects property 'name' is String",() =>{
+    var result = groceristar.getUsers();
+    result.forEach((obj) => {
+      expect(obj).toMatchObject({
+        name: expect.any(String)
+      });
+    })
+  })
+
+  test("objects property 'email' is String",() =>{
+    var result = groceristar.getUsers();
+    result.forEach((obj) => {
+      expect(obj).toMatchObject({
+        email: expect.any(String)
+      });
+    })
+  })
+
+  test("objects property 'password' is String",() =>{
+    var result = groceristar.getUsers();
+    result.forEach((obj) => {
+      expect(obj).toMatchObject({
+        password: expect.any(String)
+      });
+    })
+  })
+
+})
