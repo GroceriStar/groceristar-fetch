8ad3d353e37bf3e2d4e4a1cc9f1dd3c476ef472e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-25 02:15:38 +0200

    +

--- jsonlint.config.js
+++
@@ -1,8 +0,0 @@
-// at this file we'll run nodejs command
-// jsonlint data/Grocery/grocery.json and other checks
-var shell = require('shelljs');
-var path  = require('path');
-
-const groceryFilePath = path.resolve(__dirname, './data/Grocery/grocery.json')
-// console.log(path.resolve(__dirname, './data/Grocery/grocery.json'));
-shell.exec("jsonlint " + groceryFilePath );
