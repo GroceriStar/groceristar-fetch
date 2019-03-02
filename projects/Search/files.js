aff7179c85cd3bd4c4c4348995bb3ca856d7be4d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-02 05:05:26 +0200

    +

--- projects/Search/files.js
+++ projects/Search/files.js
@@ -1,25 +1,25 @@
 // yes it's bad, but it works, some sort of...
 //@TODO https://github.com/GroceriStar/sd/issues/3
 const allergies     =
-  require('@groceristar/sd-wrapper/lib/data/Allergy/allergies.json');
+  require('@groceristar/sd-wrapper/dist/data/Allergy/allergies.json');
 
 const courses       =
-  require('@groceristar/sd-wrapper/lib/data/Course/courses.json');
+  require('@groceristar/sd-wrapper/dist/data/Course/courses.json');
 
 const cuisines      =
-  require('@groceristar/sd-wrapper/lib/data/Cuisine/cuisines.json');
+  require('@groceristar/sd-wrapper/dist/data/Cuisine/cuisines.json');
 
 const diets         =
-  require('@groceristar/sd-wrapper/lib/data/Diet/diets.json');
+  require('@groceristar/sd-wrapper/dist/data/Diet/diets.json');
 
 const holidays      =
-  require('@groceristar/sd-wrapper/lib/data/Holiday/holidays.json');
+  require('@groceristar/sd-wrapper/dist/data/Holiday/holidays.json');
 
 const ingredients1  =
-  require('@groceristar/sd-wrapper/lib/data/Ingredients/ingredients1.json');
-  
+  require('@groceristar/sd-wrapper/dist/data/Ingredients/ingredients1.json');
+
 const measurements  =
-  require('@groceristar/sd-wrapper/lib/data/Measurement/measurements.json');
+  require('@groceristar/sd-wrapper/dist/data/Measurement/measurements.json');
 
 
 module.exports = {
