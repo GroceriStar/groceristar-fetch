676c24f2938e508c6cbb27de77e91ba23d672d80
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-20 02:45:44 +0200

    1

--- src/projects/MealCalendar/files.js
+++ src/projects/MealCalendar/files.js
@@ -3,7 +3,7 @@ const staticData = require('@groceristar/sd-wrapper')
 const mealCalendar = staticData.mealCalendar
 
 const firstVeganGLMC =
-  require('@groceristar/sd-wrapper/dist/data/MealCalendar//First-Vegan-Grocery-List-MC.json')
+  require('@groceristar/sd-wrapper/dist/data/MealCalendar/First-Vegan-Grocery-List-MC.json')
 
 module.exports = {
   mealCalendar,
