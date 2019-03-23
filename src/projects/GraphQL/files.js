9f1e27169c6ff9788100f2463c05f327c58bb072
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-23 08:08:54 +0200

    Update files.js

--- src/projects/GraphQL/files.js
+++ src/projects/GraphQL/files.js
@@ -2,10 +2,8 @@ const staticData = require('@groceristar/sd-wrapper')
 const groceristar = require('../GroceriStar/groceristar')
 
 const favorites = staticData.favorites
-  // require('@groceristar/sd-wrapper/dist/data/Ingredients/favorites.json')
 
 const items = staticData.items
-  // require('@groceristar//sd-wrapper/dist/data/Ingredients/items.json')
 
 const userGrocery = staticData.userGrocery
   // require('@groceristar/sd-wrapper/dist/data/Users/userGrocery.json')
