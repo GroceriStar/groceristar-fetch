71259f12cfe685eab3007a548daac024be3156ea
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-23 08:08:33 +0200

    Update files.js

--- src/projects/ChickenKyiv/files.js
+++ src/projects/ChickenKyiv/files.js
@@ -1,7 +1,6 @@
 const staticData = require('@groceristar/sd-wrapper')
 
 const ingredients3 = staticData.ingredients3
-  // require('@groceristar/sd-wrapper/dist/data/Ingredients/ingredients3.json')
 
 const menus = staticData.menu
 
@@ -9,10 +8,8 @@ const recipes = staticData.recipes
   // require('@groceristar/sd-wrapper/dist/data/Recipe4/recipe.json')
 
 const nutritions1 = staticData.nutritions
-// require('@groceristar/sd-wrapper/dist/data/Nutrition/nutritions1.json')
 
 const nutritions2 = staticData.nutritions2
-  // require('@groceristar/sd-wrapper/dist/data/Nutrition/nutritions2.json')
 
 const departments = staticData.departments
 
