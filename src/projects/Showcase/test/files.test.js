82e3ed67b60848adc53c58a84d3af9d70c0fd8bf
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-27 00:43:37 +0200

    1

--- src/projects/Showcase/test/files.test.js
+++ src/projects/Showcase/test/files.test.js
@@ -5,3 +5,15 @@ const showcase = require('../showcase.js')
 const methodsList = {
   getGrocery: showcase.__find('groceries', showcase.files)
 }
+
+describe('testing static data files returns array', () => {
+  const testFileIsNotEmpty = (method) => {
+    test('method {$method}()', () => {
+      var result = methodsList[method]
+      expect(result).not.toBe('')
+    })
+  }
+
+  testFileIsNotEmpty('getGrocery')
+
+})
