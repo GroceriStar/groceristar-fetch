40bf6c346e7f94e2532a80453a7841d572619484
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-29 20:59:29 +0200

    1

--- src/projects/GroceriStar/groceristar.js
+++ src/projects/GroceriStar/groceristar.js
@@ -61,22 +61,22 @@ const getGroceryByNameWithDepAndIng = function (name) {
   //@TODO maybe we shall move this function away. OR have another method, not getGrocery, that will repack things as we need it here....
   // we did few times a similar manipulations
   // for example, first step will be to use this: getGroceryByName
-  // let NEW_grocerieszzz = _.filter(grocerieszzz,
-  //   item => {
-  //     return item.name === name
-  //   })
+  let NEW_grocerieszzz = _.filter(grocerieszzz,
+    item => {
+      return item.name === name
+    })
 
-    let zizua = getGroceryByName(name);
-    let result = []
-    zizua['departments'].forEach(  function (department) {
-
-
-        result.push({
-          'department': department,
-          'ingredients': getAllIngredientsByOneDepartment(department)
-        })
-      })
-      return result
+    // let zizua = getGroceryByName(name);
+    // let result = []
+    // zizua['departments'].forEach(  function (department) {
+    //
+    //
+    //     result.push({
+    //       'department': department,
+    //       'ingredients': getAllIngredientsByOneDepartment(department)
+    //     })
+    //   })
+    //   return result
     // return NEW_grocerieszzz;
   // console.log(NEW_grocerieszzz);
 
@@ -84,19 +84,19 @@ const getGroceryByNameWithDepAndIng = function (name) {
 
 //-----
 
-  // let result = []
-  // // maybe instead of getting all groceries from getGrocery. because...
-  // // it's just a bad turn around @TODO change that.
-  // NEW_grocerieszzz[0]['departments'].forEach(
-  //   function (department) {
-  //     // @TODO add let ingredients = const getAllIngredientsByOneDepartment(department)
-  //
-  //     result.push({
-  //       'department': department,
-  //       'ingredients': getAllIngredientsByOneDepartment(department)
-  //     })
-  //   })
-  // return result
+  let result = []
+  // maybe instead of getting all groceries from getGrocery. because...
+  // it's just a bad turn around @TODO change that.
+  NEW_grocerieszzz[0]['departments'].forEach(
+    function (department) {
+      // @TODO add let ingredients = const getAllIngredientsByOneDepartment(department)
+
+      result.push({
+        'department': department,
+        'ingredients': getAllIngredientsByOneDepartment(department)
+      })
+    })
+  return result
 }
 
 // Where we're using this methods?
