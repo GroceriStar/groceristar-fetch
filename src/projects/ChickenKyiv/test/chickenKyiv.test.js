d3a6be967d8b912eb602460067664765d50e248f
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-04-02 20:33:01 +0300

    tests have same look

--- src/projects/ChickenKyiv/test/chickenKyiv.test.js
+++ src/projects/ChickenKyiv/test/chickenKyiv.test.js
@@ -2,23 +2,25 @@
 const chickenKyiv = require('../chickenKyiv.js')
 
 describe('testing static data files are set', () => {
-  test('generate basic Weekly Menu objects', () => {
-    expect(chickenKyiv.getMenuGenerator(1)).not.toBe('')
-  })
 
-  test('fetching random recipe method is set', () => {
-    expect(chickenKyiv.getRandomRecipe()).not.toBe('')
-  })
+  const methodList = {
+    getMenuGenerator: chickenKyiv.getMenuGenerator(1),
+    getRandomRecipe: chickenKyiv.getRandomRecipe(),
+    getRecipeByTitle: chickenKyiv.getRecipeByTitle('Lemonade'),
+    getFiveRandomIngredients: chickenKyiv.getFiveRandomIngredients(),
+    getFirstFiveRecipes: chickenKyiv.getFirstFiveRecipes(),
+  }
 
-  test('recipe method getting by Title is set', () => {
-    expect(chickenKyiv.getRecipeByTitle('Lemonade')).not.toBe('')
-  })
+  methodReturnsArray = method => {
+    test(`chickenKyiv method ${} returns array`, () => {
+      const result = methodList[method]
+      expect(Array.isArray(result)).toBe(true)
+    })
+  }
 
-  test('getFiveRandomIngredients', () => {
-    expect(chickenKyiv.getFiveRandomIngredients()).not.toBe('')
-  })
-
-  test('getFirstFiveRecipes method', () => {
-    expect(chickenKyiv.getFirstFiveRecipes()).not.toBe('')
-  })
+  methodReturnsArray('getMenuGenerator')
+  methodReturnsArray('getRandomRecipe')
+  methodReturnsArray('getRecipeByTitle')
+  methodReturnsArray('getFiveRandomIngredients')
+  methodReturnsArray('getFirstFiveRecipes')
 })
