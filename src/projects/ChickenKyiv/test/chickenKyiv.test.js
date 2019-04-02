e286485522f27fb7c0508858caa4fddb5eaba53a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-04-02 20:28:42 +0300

    Update chickenKyiv.test.js

--- src/projects/ChickenKyiv/test/chickenKyiv.test.js
+++ src/projects/ChickenKyiv/test/chickenKyiv.test.js
@@ -1,6 +1,8 @@
 /* global test, describe, it, expect, jest */
 const chickenKyiv = require('../chickenKyiv.js')
 
+
+// @TODO OK, but i don't like when you put everything in long line inside of your ass @hirdbluebird. Looks not better.
 describe('testing static data files are set', () => {
   test('generate basic Weekly Menu objects', () => {
     expect(chickenKyiv.getMenuGenerator(1)).not.toBe('')
