4dc15c1a9566d57380844aadca22c8014bd5622d
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-04-06 14:09:41 +0300

    graphQL tests

--- src/projects/GroceriStar/test/gsUsers.test.js
+++ src/projects/GroceriStar/test/gsUsers.test.js
@@ -18,45 +18,4 @@ describe('testing objects inside users', () => {
   userObjectIsValid('email')
   userObjectIsValid('password')
   userObjectIsValid('password')
-
-  // test("each object have property 'email'", () => {
-  //   var result = groceristar.getUsers()
-  //   result.forEach((obj) => {
-  //     expect(obj).toHaveProperty('email')
-  //   })
-  // })
-  //
-  // test("each object have property 'password'", () => {
-  //   var result = groceristar.getUsers()
-  //   result.forEach((obj) => {
-  //     expect(obj).toHaveProperty('password')
-  //   })
-  // })
-  //
-  // test("objects property 'name' is String", () => {
-  //   var result = groceristar.getUsers()
-  //   result.forEach((obj) => {
-  //     expect(obj).toMatchObject({
-  //       name: expect.any(String)
-  //     })
-  //   })
-  // })
-  //
-  // test("objects property 'email' is String", () => {
-  //   var result = groceristar.getUsers()
-  //   result.forEach((obj) => {
-  //     expect(obj).toMatchObject({
-  //       email: expect.any(String)
-  //     })
-  //   })
-  // })
-  //
-  // test("objects property 'password' is String", () => {
-  //   var result = groceristar.getUsers()
-  //   result.forEach((obj) => {
-  //     expect(obj).toMatchObject({
-  //       password: expect.any(String)
-  //     })
-  //   })
-  // })
 })
