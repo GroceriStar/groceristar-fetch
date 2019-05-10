16a7e13c6a37c967814dfc8e4d5e0576de0a7c13
Benitha <ebi.benny@gmail.com>
2019-05-10 11:10:16 -0700

    updated UnitConversion/files.test.js
    Tests are added to check objects are not empty

--- src/projects/UnitConvertion/files.test.js
+++ src/projects/UnitConvertion/files.test.js
@@ -1,7 +1,23 @@
+const files= require('./files.js');
+const measurementUnits = files.measurementUnits;
+const measurementSystems = files.measurementSystems;
+
 /* global test, describe, it, expect, jest */
 
 describe('empty test', () => {
   test.skip('it is not snowing', () => {
     expect(false).toBe(0)
   })
+})
+
+describe('testing objects are not empty', () => {
+  const objectIsNotEmpty = (object) => {
+    test('object ' + object, () => {
+      var result = object
+      expect(result).not.toBe('')
+    })
+  }
+objectIsNotEmpty(measurementUnits)
+objectIsNotEmpty(measurementSystems)
+
 })
