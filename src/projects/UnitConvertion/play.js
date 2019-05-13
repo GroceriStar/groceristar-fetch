2c413f15bd018048939b0f32641abb29e30c8ba1
Benitha <ebi.benny@gmail.com>
2019-05-12 23:41:26 -0700

    Created and updated play.js

---
+++ src/projects/UnitConvertion/play.js
@@ -0,0 +1,11 @@
+const objects = {
+         measurementSystems, 
+         measurementUnits,
+         generatedMeasurementUnits,
+         generatedMeasurementSystem
+         } = require('./files')
+         
+console.log('measurementSystems: '+ measurementSystems)
+console.log('measurementUnits: '+ measurementUnits)
+console.log('generatedMeasurementUnits: '+ generatedMeasurementUnits)
+console.log('generatedMeasurementSystem: + generatedMeasurementSystem)
