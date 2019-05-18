1833dcacf10bca65177be28fa1ff72a0e47bfc76
elnur004 <elnur@Macbook.local>
2019-05-18 04:32:58 +0400

    added some files

--- src/projects/UnitConvertion/files.js
+++ src/projects/UnitConvertion/files.js
@@ -8,6 +8,33 @@ const generatedMeasurementUnits = staticData.generatedMeasurementUnits;
 
 const generatedMeasurementSystem = staticData.generatedMeasurementSystem;
 
+const cooking_times = staticData.cooking_times;
+
+const cup1 = staticData.cup1;
+
+const cup1_2 = staticData.cup1_2;
+
+const cup1_4 = staticData.cup1_4;
+
+const cup1_8 = staticData.cup1_8;
+
+const cup3_4 = staticData.cup3_4;
+
+const dash = staticData.dash;
+
+const gallon1 = staticData.gallon1;
+
+const pinch = staticData.pinch;
+
+const pint1 = staticData.pint1;
+
+const quart1 = staticData.quart1;
+
+const tablespoon_1 = staticData.tablespoon_1;
+
+const teaspoon_1 = staticData.teaspoon_1;
+
+
 module.exports = {
 
     measurementSystems,
@@ -16,8 +43,32 @@ module.exports = {
     
     generatedMeasurementUnits,
     
-    generatedMeasurementSystem
+    generatedMeasurementSystem,
     
-    
-  
+    cooking_times,
+
+    cup1,
+
+    cup1_2,
+
+    cup1_4,
+
+    cup1_8,
+
+    cup3_4,
+
+    dash,
+
+    gallon1,
+
+    pinch,
+
+    pint1,
+
+    quart1,
+
+    tablespoon_1,
+
+    teaspoon_1
+
     }
