ef76cd2ad5abb89a9a3db1f44c7fdd5032886941
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-05-21 22:50:08 +0300

    standard code-fix

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-/* global test, describe, it, expect, jest */
+/* global test, describe, it, expect */
 // @TODO we need to have all "projects"
 const chickenKyiv = require('./projects/ChickenKyiv/chickenKyiv.js')
 const graphQL = require('./projects/GraphQL/graphQL.js')
@@ -18,12 +18,12 @@ describe('testing if methods inside the files are accessible from the outside', 
   //   expect(stream).not.toBe('')
   // })
 
-  test('[ChickenKyiv] get recipe by title file is set', () => {
+  it('[ChickenKyiv] get recipe by title file is set', () => {
     var stream = chickenKyiv.getRecipeByTitle('Lemonade')
     expect(stream).not.toBe('')
   })
 
-  test('[ChickenKyiv] fetch random recipe file is set', () => {
+  it('[ChickenKyiv] fetch random recipe file is set', () => {
     var stream = chickenKyiv.getRandomRecipe()
     expect(stream).not.toBe('')
   })
@@ -37,22 +37,22 @@ describe('testing if methods inside the files are accessible from the outside', 
   //   expect(stream).not.toBe('')
   // })
 
-  test('[GS-Loopback] fetch Ultimate Grocery List', () => {
+  it('[GS-Loopback] fetch Ultimate Grocery List', () => {
     var stream = loopback.getUltimateGrocery()
     expect(stream).not.toBe('')
   })
 
-  test('[GS-Loopback] fetch grocery with user relations', () => {
+  it('[GS-Loopback] fetch grocery with user relations', () => {
     var stream = loopback.getGLwithUserRelations()
     expect(stream).not.toBe('')
   })
 
-  test('[GS-Loopback] get loopback sample data', () => {
+  it('[GS-Loopback] get loopback sample data', () => {
     var stream = loopback.getIngredientsSampleFromDB()
     expect(stream).not.toBe('')
   })
 
-  test('[GS-Loopback] fetch a useronject data with a name argument in loopback ', () => {
+  it('[GS-Loopback] fetch a useronject data with a name argument in loopback ', () => {
     var stream = loopback.getUserObject('Vadim')
     expect(stream).not.toBe('')
   })
@@ -61,73 +61,73 @@ describe('testing if methods inside the files are accessible from the outside', 
 // graphQL methods check
 describe('testing if methods inside the files are accessible from the outside', () => {
   // test for getDepartments method
-  test('[GraphQL] fetch list of departments', () => {
+  it('[GraphQL] fetch list of departments', () => {
     var stream = graphQL.getDepartments()
     expect(stream).not.toBe('')
   })
 
   // test for getDepartmentsKey method
-  test('[GraphQL] fetch keys for department', () => {
+  it('[GraphQL] fetch keys for department', () => {
     var stream = graphQL.getDepartmentsKey()
     expect(stream).not.toBe('')
   })
 
   // test for getGrocery method
-  test('[GraphQL] get grocery', () => {
+  it('[GraphQL] get grocery', () => {
     var stream = graphQL.getGrocery()
     expect(stream).not.toBe('')
   })
 
   // test for getGroceryKey method
-  test('[GraphQL] fetch a keys from a grocery', () => {
+  it('[GraphQL] fetch a keys from a grocery', () => {
     var stream = graphQL.getGroceryKey()
     expect(stream).not.toBe('')
   })
 
   // test for getIngredients method
-  test('[GraphQL] fetch a list of ingredient', () => {
+  it('[GraphQL] fetch a list of ingredient', () => {
     var stream = graphQL.getIngredients()
     expect(stream).not.toBe('')
   })
 
   // test for getIngredientsKey method
-  test('[GraphQL] fetch a list keys of ingredient', () => {
+  it('[GraphQL] fetch a list keys of ingredient', () => {
     var stream = graphQL.getIngredientsKey()
     expect(stream).not.toBe('')
   })
 
   // test for getUsers method
-  test('[GraphQL] fetch a list of User', () => {
+  it('[GraphQL] fetch a list of User', () => {
     var stream = graphQL.getUsers()
     expect(stream).not.toBe('')
   })
 
   // test for getUsersKey method
-  test('[GraphQL] fetch a list keys of users', () => {
+  it('[GraphQL] fetch a list keys of users', () => {
     var stream = graphQL.getUsersKey()
     expect(stream).not.toBe('')
   })
 
   // test for getFavorites method
-  test('[GraphQL] fetch a list of favorite', () => {
+  it('[GraphQL] fetch a list of favorite', () => {
     var stream = graphQL.getFavorites()
     expect(stream).not.toBe('')
   })
 
   // test for getItems method
-  test('[GraphQL] fetch a list of item', () => {
+  it('[GraphQL] fetch a list of item', () => {
     var stream = graphQL.getItems()
     expect(stream).not.toBe('')
   })
 
   // test for getUserGrocery method
-  test('[GraphQL] gets a grocies of user', () => {
+  it('[GraphQL] gets a grocies of user', () => {
     var stream = graphQL.getUserGrocery()
     expect(stream).not.toBe('')
   })
 
   // test for getFavoritesKey method
-  test('[GraphQL] fetch a list keys of favorite', () => {
+  it('[GraphQL] fetch a list keys of favorite', () => {
     var stream = graphQL.getFavoritesKey()
     expect(stream).not.toBe('')
   })
@@ -139,7 +139,7 @@ describe('testing if methods inside the files are accessible from the outside', 
   })
 
   // test for getUserGroceryKey method
-  test('[GraphQL] fetch a list keys of user grocery', () => {
+  it('[GraphQL] fetch a list keys of user grocery', () => {
     var stream = graphQL.getUserGroceryKey()
     expect(stream).not.toBe('')
   })
@@ -147,12 +147,12 @@ describe('testing if methods inside the files are accessible from the outside', 
 
 describe('Groceristar methods check', () => {
   // @TODO format it later
-  test('[Groceristar] departments file is set', () => {
+  it('[Groceristar] departments file is set', () => {
     var stream = groceristar.getDepartments()
     expect(stream).not.toBe('')
   })
 
-  test('[Groceristar] showcase checking', () => {
+  it('[Groceristar] showcase checking', () => {
     var data = showcase.getGroceryShowcase()
     expect(data).not.toBe('')
   })
@@ -162,19 +162,19 @@ describe('Groceristar methods check', () => {
 describe('MealCalendar methods check', () => {
   // @TODO format it later
   // test for getDishByDay method
-  test('[MealCalendar] gets dish by day', () => {
+  it('[MealCalendar] gets dish by day', () => {
     var stream = mealCalendar.getDishByDay('Monday')
     expect(stream).not.toBe('')
   })
 
   // test for getDishByMonth method
-  test('[MealCalendar] gets dish by Month', () => {
+  it('[MealCalendar] gets dish by Month', () => {
     var data = mealCalendar.getDishByMonth(2)
     expect(data).not.toBe('')
   })
 
   // test for getDishByWeek method
-  test('[MealCalendar] gets dish by Week', () => {
+  it('[MealCalendar] gets dish by Week', () => {
     var data = mealCalendar.getDishByWeek(3)
     expect(data).not.toBe('')
   })
@@ -182,7 +182,7 @@ describe('MealCalendar methods check', () => {
 
 describe('Search Form methods check', () => {
   // @TODO format it later
-  test('[Search Form] getAttribute("allergy")', () => {
+  it('[Search Form] getAttribute("allergy")', () => {
     var stream = search.getAttribute('allergy')
     expect(stream).not.toBe('')
   })
@@ -193,7 +193,7 @@ describe('ShowCase methods check', () => {
   // @TODO format it later
 
   // testing getGroceryShowcase method isnt empty
-  test('[ShowCase] showcase checking', () => {
+  it('[ShowCase] showcase checking', () => {
     var stream = showcase.getGroceryShowcase()
     expect(stream).not.toBe('')
   })
--- src/projects/UnitConvertion/files.test.js
+++ src/projects/UnitConvertion/files.test.js
@@ -6,21 +6,21 @@ const measurementSystems = files.measurementSystems
 const generatedMeasurementUnits = files.generatedMeasurementUnits
 const generatedMeasurementSystem = files.generatedMeasurementSystem
 
-const cooking_times = files.cooking_times
+const cookingTimes = files.cookingTimes
 const cup1 = files.cup1
-const cup1_2 = files.cup1_2
-const cup1_4 = files.cup1_4
-const cup1_8 = files.cup1_8
-const cup3_4 = files.cup3_4
+const cup12 = files.cup12
+const cup14 = files.cup14
+const cup18 = files.cup18
+const cup34 = files.cup34
 const dash = files.dash
 const gallon1 = files.gallon1
 const pinch = files.pinch
 const pint1 = files.pint1
 const quart1 = files.quart1
-const tablespoon_1 = files.tablespoon_1
-const teaspoon_1 = files.teaspoon_1
+const tablespoon1 = files.tablespoon1
+const teaspoon1 = files.teaspoon1
 
-/* global test, describe, it, expect, jest */
+/* global test, describe, it, expect, */
 
 describe('empty test', () => {
   test.skip('it is not snowing', () => {
@@ -30,7 +30,7 @@ describe('empty test', () => {
 
 describe('testing objects are not empty', () => {
   const objectIsNotEmpty = (object) => {
-    test('object ' + object, () => {
+    it('object ' + object, () => {
       var result = object
       expect(result).not.toBe('')
     })
@@ -40,24 +40,24 @@ describe('testing objects are not empty', () => {
   objectIsNotEmpty(measurementSystems)
   objectIsNotEmpty(generatedMeasurementUnits)
   objectIsNotEmpty(generatedMeasurementSystem)
-  objectIsNotEmpty(cooking_times)
+  objectIsNotEmpty(cookingTimes)
   objectIsNotEmpty(cup1)
-  objectIsNotEmpty(cup1_2)
-  objectIsNotEmpty(cup1_4)
-  objectIsNotEmpty(cup1_8)
-  objectIsNotEmpty(cup3_4)
+  objectIsNotEmpty(cup12)
+  objectIsNotEmpty(cup14)
+  objectIsNotEmpty(cup18)
+  objectIsNotEmpty(cup34)
   objectIsNotEmpty(dash)
   objectIsNotEmpty(gallon1)
   objectIsNotEmpty(pinch)
   objectIsNotEmpty(pint1)
   objectIsNotEmpty(quart1)
-  objectIsNotEmpty(tablespoon_1)
-  objectIsNotEmpty(teaspoon_1)
+  objectIsNotEmpty(tablespoon1)
+  objectIsNotEmpty(teaspoon1)
 })
 
 describe('testing object returns array', () => {
   const isArray = (object) => {
-    test('object' + object, () => {
+    it('object' + object, () => {
       var result = object
       expect(Array.isArray(result)).toBe(true)
     })
@@ -67,17 +67,17 @@ describe('testing object returns array', () => {
   isArray(measurementSystems)
   isArray(generatedMeasurementUnits)
   isArray(generatedMeasurementSystem)
-  isArray(cooking_times)
+  isArray(cookingTimes)
   isArray(cup1)
-  isArray(cup1_2)
-  isArray(cup1_4)
-  isArray(cup1_8)
-  isArray(cup3_4)
+  isArray(cup12)
+  isArray(cup14)
+  isArray(cup18)
+  isArray(cup34)
   isArray(dash)
   isArray(gallon1)
   isArray(pinch)
   isArray(pint1)
   isArray(quart1)
-  isArray(tablespoon_1)
-  isArray(teaspoon_1)
+  isArray(tablespoon1)
+  isArray(teaspoon1)
 })
