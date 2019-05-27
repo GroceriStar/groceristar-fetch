ac99407c7fe1a349e3f4bef68ae2bd534b968b24
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-05-28 01:13:55 +0300

    graphQL test fixes

--- src/projects/GraphQL/graphQL.js
+++ src/projects/GraphQL/graphQL.js
@@ -7,13 +7,13 @@ const {
   __generateDate
 } = require('../../utils')
 
-// const files = {
-//   groceristar,
-//   groceries,
-//   favorites,
-//   items,
-//   userGrocery
-// } = require('./files')
+const {
+  favorites,
+  items
+  // groceristar,
+  // groceries,
+  // userGrocery
+} = require('./files')
 
 // @TODO return measurements
 
@@ -32,36 +32,30 @@ const {
 // }
 
 const getFavorites = function () {
-  // let favorites = favorites
-
-  // return _.map(favorites, (favorite, index) => {
-  //   return {
-  //     key: ++index,
-  //     ...favorite
-  //   }
-  // })
+  return _.map(favorites, (favorite, index) => {
+    return {
+      key: ++index,
+      ...favorite
+    }
+  })
 }
 
 const getFavoritesKey = function () {
-  // let favorites = getFavorites()
-
-  // return _.map(favorites, (favorite, index) => {
-  //   return {
-  //     key: __generateId(),
-  //     ...favorite
-  //   }
-  // })
+  return _.map(favorites, (favorite, index) => {
+    return {
+      key: __generateId(),
+      ...favorite
+    }
+  })
 }
 
 const getItems = function () {
-  // let favorites = getItems()
-
-  // return _.map(items, (item, index) => {
-  //   return {
-  //     key: ++index,
-  //     ...item
-  //   }
-  // })
+  return _.map(items, (item, index) => {
+    return {
+      key: ++index,
+      ...item
+    }
+  })
 }
 
 const getItemsKey = function () {
