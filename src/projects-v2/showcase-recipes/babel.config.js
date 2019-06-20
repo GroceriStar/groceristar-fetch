daaf4073eb97c7649e8e7e4cc969a67a653f6d42
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-20 04:12:15 +0300

    showcase-recipes

---
+++ src/projects-v2/showcase-recipes/babel.config.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// const path = require('path')
+
+const presets = [
+  ['@babel/preset-env', {
+    'modules': false
+  }]
+  // {
+  //
+  // }
+]
+
+const plugins = [
+  [
+    require.resolve('babel-plugin-module-resolver'),
+    {
+      root: ['./src/'],
+      alias: {
+        '~': './src/data'
+      }
+    }
+  ],
+  [
+    '@babel/plugin-syntax-dynamic-import'
+  ]
+  // ["@babel/proposal-object-rest-spread"]
+]
+
+module.exports = { presets, plugins }
