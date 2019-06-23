b1b6397337197a42345757bcc39d3c8cedf2f3de
seeni <seeni0424@gmail.com>
2019-06-23 18:35:16 +0530

    UPES6: changed require and export to import and export

---
+++ src/projects-v2/MealCalendar/src/mealCalendar.js
@@ -0,0 +1,18 @@
+var getDishByWeek = function (week){
+    //@TODO implement the business logic
+    return {"STATUS":"NOT IMPLEMENTED"};
+}
+var getDishByMonth= function (month){
+    //@TODO implement the business logic
+    return {"STATUS":"NOT IMPLEMENTED"};
+}
+var getDishByDay = function (day){
+    //@TODO implement the business logic
+    return {"STATUS":"NOT IMPLEMENTED"};
+}
+
+export {
+    getDishByMonth,
+    getDishByWeek,
+    getDishByDay
+}
