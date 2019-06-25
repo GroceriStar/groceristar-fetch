aed6e4d928228e1b1c114a5b4270c0ff41f6857a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-25 10:58:41 +0300

    lodash update

--- src/projects-v2/GroceryLists/rollup.config.js
+++ src/projects-v2/GroceryLists/rollup.config.js
@@ -19,7 +19,7 @@ const name = 'GroceristarFetchPlugin'
 const { external, globals } = {
   'globals': {
     'fs': 'fs',
-    '_': 'lodash',
+    'lodash': '_',
     'path': 'path',
     'dayjs':'dayjs',
     'uuid': 'uuid'
