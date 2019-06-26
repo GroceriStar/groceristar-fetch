a54d3badc6741a2a07dfcba6a5eab10a8a5798c6
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-26 05:37:55 +0300

    loopback

--- src/projects-v2/GSLoopback/dist/play.js
+++
@@ -1,4 +0,0 @@
-import { getUltimateGrocery, getGLwithUserRelations, getIngredientsSampleFromDB } from "./index";
-console.log(getUltimateGrocery());
-console.log(getGLwithUserRelations());
-console.log(getIngredientsSampleFromDB());
