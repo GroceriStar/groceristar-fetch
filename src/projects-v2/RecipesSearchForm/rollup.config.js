bf3af44162551b0f7ee1aebc43ee1a9790a7f435
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-27 19:18:44 +0300

    clean up

--- src/projects-v2/RecipesSearchForm/rollup.config.js
+++ src/projects-v2/RecipesSearchForm/rollup.config.js
@@ -49,23 +49,10 @@ export default {
     }),
 
     // Allow bundling cjs modules. Rollup doesn't understand cjs
-    commonjs({
-      // namedExports: {
-      // //   // left-hand side can be an absolute path, a path
-      // //   // relative to the current directory, or the name
-      // //   // of a module in node_modules
-      // //   'node_modules/my-lib/index.js': [ 'named' ]
-      //   '~/Allergy/allergies.json': [ 'allergies' ]
-      // }
-    }),
+    commonjs(),
     builtins()
 
-    // juck fix in order to move json files to npm
-    // copy({
-    //   assets: [
-    //     './src/data/**'
-    //   ],
-    // }),
+    
 
   ],
 
