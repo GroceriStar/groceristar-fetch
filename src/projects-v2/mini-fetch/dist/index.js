10ab759ae6060e475a2f126f566d87385de4cb5a
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-06-29 18:01:12 +0300

    release again

--- src/projects-v2/mini-fetch/dist/index.js
+++
@@ -1,8 +0,0 @@
-var groceristar = require("./projects/GroceriStar/groceristar.js");
-
-var chickenKyiv = require("./projects/ChickenKyiv/chickenKyiv.js");
-
-module.exports = {
-  groceristar: groceristar,
-  chickenKyiv: chickenKyiv
-};
