50988037710f044f617a6a727e8faf35ae677957
elnur004 <elnur@Macbook.local>
2019-07-01 01:57:02 +0400

    generated favorites

--- docs/const-departments-example.js
+++ docs/const-departments-example.js
@@ -15,15 +15,3 @@ export const SHOW_ALL_DEPARTMENTS_METHOD = "SHOW_ALL_DEPARTMENTS_METHOD";
 
 //For method, that will removing department from grocery list
 export const REMOVE_DEPARTMENT_FROM_GROCERY_LIST = "REMOVE_DEPARTMENT_FROM_GROCERY_LIST";
-
-//For method, that gets all ingredients with status favorite
-export const GET_ALL_FAVORITE_INGREDIENTS = 'GET_ALL_FAVORITE_INGREDIENTS';
-
-//For method, that deletes ingredients from favorites list
-export const DELETE_INGREDIENTS_FROM_FAVORITES_LIST = 'DELETE_INGREDIENTS_FROM_FAVORITES_LIST';
-
-//For adding favorites
-export const ADD_FAVORITES = 'ADD_FAVORITES';
-
-//For method, that lists all favorites
-export const LIST_ALL_FAVORITES = 'LIST_ALL_FAVORITES';
---
+++ docs/favorites.js
@@ -0,0 +1,11 @@
+//For method, that gets all ingredients with status favorite
+export const GET_ALL_FAVORITE_INGREDIENTS = 'GET_ALL_FAVORITE_INGREDIENTS';
+
+//For method, that deletes ingredients from favorites list
+export const DELETE_INGREDIENTS_FROM_FAVORITES_LIST = 'DELETE_INGREDIENTS_FROM_FAVORITES_LIST';
+
+//For adding favorites
+export const ADD_FAVORITES = 'ADD_FAVORITES';
+
+//For method, that lists all favorites
+export const LIST_ALL_FAVORITES = 'LIST_ALL_FAVORITES';
