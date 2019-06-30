92adf73e9fd09a4977794ba6d8cfea864f1eceae
elnur004 <elnur@Macbook.local>
2019-07-01 02:45:26 +0400

    generated grocery.js

---
+++ docs/grocery.js
@@ -0,0 +1,23 @@
+//Method that will Display Grocery List Data
+export const GET_FULL_GROCERY_LIST = "GET_FULL_GROCERY_LIST";
+
+//Method that will Attach grocery list to user
+export const ATTACH_GROCERY_LIST_TO_USER = 'ATTACH_GROCERY_LIST_TO_USER';
+
+//Clone list with all data to a new grocery list record
+export const CLONE_ALL_DATA_TO_A_NEW_GROCERY_LIST = 'CLONE_ALL_DATA_TO_A_NEW_GROCERY_LIST';
+
+//Display page with clone Form
+export const GET_PAGE_WITH_CLONE_FORM = 'GET_PAGE_WITH_CLONE_FORM';
+
+//Remove Grocery list from user account	
+export const REMOVE_GROCERY_LIST_FROM_USER = 'REMOVE_GROCERY_LIST_FROM_USER';
+
+//Create New, Empty Grocery List inside the user account
+export const GET_NEW_EMPTY_GROCERY_LIST = 'GET_NEW_EMPTY_GROCERY_LIST';
+
+//page with Method, that will display all grocery lists, related to user
+export const GET_ALL_GROCERY_LISTS_RELETED_TO_USER = 'GET_ALL_GROCERY_LISTS_RELETED_TO_USER';
+
+//Method that will update the grocery list name
+export const UPDATE_GROCERY_LIST_NAME = 'UPDATE_GROCERY_LIST_NAME';
