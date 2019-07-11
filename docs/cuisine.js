915e21645c92644cf670fc9920ced95aa734fe98
elnur004 <elnur@Macbook.local>
2019-07-12 00:51:02 +0400

    created const for methods and models

---
+++ docs/cuisine.js
@@ -0,0 +1,14 @@
+//http://localhost:3000/api/attribute?filter[where][type]=cuisine
+export const ATTRIBUTE_FILTER_TYPE_CUISINE = 'ATTRIBUTE_FILTER_TYPE_CUISINE';
+
+//http://localhost:3000/api/attribute?filter[where][and][][type]=cuisine
+export const ATTRIBUTE_FILTER_AND_TYPE_CUISINE = 'ATTRIBUTE_FILTER_AND_TYPE_CUISINE';
+
+//http://localhost:3000/api/attribute?filter[where][id]=5abc4e9da2738950031da8b0
+export const ATTRIBUTE_FILTER_ID = 'ATTRIBUTE_FILTER_ID';
+
+//http://localhost:3000/api/attribute?filter[where][and][][type]=cuisine&filter[where][and][][name]=American
+export const ATTRIBUTE_FILTER_AND_TYPE_CUISINE_FILTER_AND_NAME = 'ATTRIBUTE_FILTER_AND_TYPE_CUISINE_FILTER_AND_NAME';
+
+//http://localhost:3000/api/attribute?filter[where][and][][type]=cuisine&filter[where][and][][id]=5abc4e9da2738950031da8b0
+export const ATTRIBUTE_FILTER_AND_TYPE_CUISINE_FILTER_AND_ID = 'ATTRIBUTE_FILTER_AND_TYPE_CUISINE_FILTER_AND_ID';
---
+++ docs/purchased.js
@@ -0,0 +1,8 @@
+//Method that toggle purchased item(ingredient)	
+export const TOGGLE_PURCHASED_ITEM = 'TOGGLE_PURCHASED_ITEM';
+
+//Method that will remove all items(ingredients) from purchased list
+export const REMOVE_ALL_ITEMS_FROM_PURCHASED_LIST = 'REMOVE_ALL_ITEMS_FROM_PURCHASED_LIST';
+
+//Method that will remove ingredient, from grocery list
+export const REMOVE_INGREDIENT_FROM_GROCERY_LIST = 'REMOVE_INGREDIENT_FROM_GROCERY_LIST';
