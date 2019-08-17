54ed6eafb4761890ded163d45cbd5d1d1ec85069
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-08-17 17:02:03 +0000

    updates

---
+++ projects/mini-fetch/src/projects/ChickenKyiv/play.js
@@ -0,0 +1,49 @@
+// const _ = require('lodash')
+
+const files = {
+  // ingredients3,
+  // menus,
+  // recipes,
+  nutritions1,
+  nutritions2,
+  departments,
+  users
+} = require('./files')
+
+console.log(nutritions1)
+console.log(nutritions2)
+console.log(departments)
+// console.log(users)
+
+
+
+
+
+// import {
+//   getRecipes,
+//   getShowcaseFiveRecipes,
+//   getNRecipes,
+//   getMenuGenerator,
+//   getRandomRecipe,
+//   getFirstFiveRecipes,
+//   getFiveRandomIngredients,
+//   getDepartments,
+//   getUsers
+// } from './index'
+
+// console.log( getShowcaseFiveRecipes() );
+
+// console.log( count(getRecipes()) );
+// console.log( getNRecipes(6) );
+
+// console.log(getRecipes())
+// console.log(getMenuGenerator(5))
+// console.log(getRandomRecipe() );
+
+// console.log(getFirstFiveRecipes() );
+
+// console.log(getFiveRandomIngredients() );
+
+// console.log(getDepartments() );
+
+// console.log(getUsers() );
